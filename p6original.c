39894dedf8b5b3c43aeecf25118a831c0bfa688d
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-02-13 11:16:15 +0000

    done

--- p6original.c
+++ p6original.c
@@ -1,43 +1,30 @@
 #include<stdio.h>
-void input_two_strings(char *a,char *b)
+void input_string(char *a)
 {
-    printf("enter the two words\n");
-    scanf("%s",a);
-    scanf("%s",b);
+  scanf("%s",a);
 }
-int strcmp(char a[], char b[])
+void str_revers(char *a, char *r)
 {
-    int w=0;
-    for(w=0; ((a[w]!=0) || (b[w]!=0)); w++)
+  int x=0;
+  for(int i=0;a[i]!=0;i++)
     {
-        if(a[w]-b[w]!=0)
-        {
-            break;
-        }
+      x=x+1;
     }
-    return a[w]-b[w];
+  for(int i=0;i<x+1;i++)
+    {
+      r[i]=a[x-i-1];
+    }
 }
-void output(char a[], char b[],int soln)
+void output(char *a,char *revers)
 {
-    if(soln>0)
-    {
-        printf("%s is greater than %s\n",a,b);
-    }
-    else if(soln<0)
-    {
-        printf("%s is greater than %s\n",b,a);
-    }
-    else
-    {
-        printf("both the words are same\n");
-    }
+  printf("%s reversed is %s\n",a,revers);
 }
 int main()
 {
-    int x;
-    char a[15],b[15];
-    input_two_strings(a,b);
-    x=strcmp(a,b);
-    output(a,b,x);
-    return(0);
+  printf("enter the string \n");
+  char a[20],r[20];
+  input_string(a);
+  str_revers(a,r);
+  output(a,r);
+  return 0;
 }
